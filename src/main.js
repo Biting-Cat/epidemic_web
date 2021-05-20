a5c429d3e0a7910aa436eaa2e1d8c9319348df0d
李宝霖 <450113828@qq.com>
2021-05-21 00:30:05 +0800

    'echars和axios的全局配置'

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+// 请求基准路径的配置（后台接口）
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api'
+// 将axios挂载到vue的原型对象上
+Vue.prototype.$http = axios
+// 将全局的echarts挂载到VUE的原型对象上，
+// 调用：this.$echarts
+Vue.prototype.$echarts = window.echarts
 
 Vue.config.productionTip = false
 
