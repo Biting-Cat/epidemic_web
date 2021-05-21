e0c70e18cf70dbc9c801d77313c554d6823008ae
李宝霖 <450113828@qq.com>
2021-05-21 14:20:50 +0800

    '病死率折线图'

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Deathratepage from '../views/Deathratepage.vue'
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/Deathratepage',
+    component: Deathratepage
+  }
 ]
 
 const router = new VueRouter({
